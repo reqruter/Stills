74f697f6749c922f297ce7181cd61a50168968c4
reqruter <reqruter@yandex.ru>
2023-03-21 18:41:24 +0300

    gitignore added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2000;
+        int mile = 20;
+        int bonus = ticketPrice / mile;
+
+        System.out.println("Сумма бонусных миль:" + bonus);
+    }
+}
